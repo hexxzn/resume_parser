779bf0a8dc99a0a0fcefbd9df0cfb7d5035db03a
George <georgekilburg@gmail.com>
2022-06-01 15:00:05 -0400

    1.2.5
    more code cleanup

--- data.py
+++ data.py
@@ -10,7 +10,7 @@ import re
 
 class Candidate:
     def __init__(self, first, last, location, email, phone, summary, skills, education, experience):
-        # Candidate Identity
+        # Candidate identity
         self.first = first.contents[0] if first else 'First Name'
         self.last = last.contents[0] if last else 'Last Name'
         self.location = location.contents[0] if location else 'Location'
@@ -18,13 +18,13 @@ class Candidate:
         self.phone = phone.contents[0] if phone else 'Phone Number'
         self.summary = summary.contents[0] if summary else 'Summary'
 
-        # Candidate Skills
+        # Candidate skills
         self.skills = '' if skills else 'Skills'
         for skill in skills:
             self.skills += format_string(skill.contents[0]) + '\n'
         self.skills = self.skills[:-1]
 
-        # Candidate Education
+        # Candidate education
         education_dict = {}
         if education:
             for item in education:
@@ -48,7 +48,7 @@ class Candidate:
             self.education += '\n'
         self.education = self.education[:-2]
 
-        # Candidate Experience
+        # Candidate experience
         experience_dict = {}
         if experience:
             for item in experience:
@@ -74,27 +74,24 @@ class Candidate:
             self.experience += '\n'
         self.experience = self.experience[:-2]
 
-# Extract Candidate Data From HTML
+# Extract candidate data from HTML.
 def main(url, manual_login):
-    driver = webdriver.Firefox() # Create driver
+    driver = webdriver.Firefox() # Create driver.
+    indeed_login(driver, url, manual_login) # Log in to Indeed.
+    source = driver.page_source # Get page source.
+    driver.close() # Close driver.
+    candidate = create_candidate(source) # Create candidate.
+    parse_document_text(candidate) # Create resume from template.
 
-    indeed_login(driver, url, manual_login) # Log in to Indeed
-    
-    source = driver.page_source # Get page source
-    driver.close()
-    
-    candidate = create_candidate(source) # Create candidate
-
-    parse_document_text(candidate) # Create resume from template
-
+# Log in to Indeed.
 def indeed_login(driver, url, manual_login):
-    #Log In Manually
+    #Log in manually if manual login is true or if cookies.pkl doesn't exist.
     if not os.path.exists('resources\cookies.pkl') or manual_login:
         driver.get(url)
         WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.rdp-resume-container'))) # Wait for resume to load before saving cookies
         if EC.presence_of_element_located((By.CSS_SELECTOR, '.rdp-resume-container')):
             pickle.dump(driver.get_cookies(), open('resources/cookies.pkl', 'wb'))
-    # Log In With Cookies
+    # Log in with saved cookies.
     else:
         driver.get(url)
         cookies = pickle.load(open('resources/cookies.pkl', 'rb'))
@@ -104,46 +101,45 @@ def indeed_login(driver, url, manual_login):
         driver.get(url)
         WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.rdp-resume-container')))
 
-# Create Candidate Instance
+# Create candidate instance.
 def create_candidate(source):
     soup = BeautifulSoup(source, "html.parser")
-
     candidate = Candidate(
-        soup.find('span', {'data-shield-id': 'firstname'}),
-        soup.find('span', {'data-shield-id': 'lastname'}),
-        soup.find('span', {'data-shield-id': 'locality'}),
-        soup.find('div', {'data-shield-id': 'email'}),
-        soup.find('div', {'data-shield-id': 'phone_number'}),
-        soup.find('div', {'data-shield-id': 'res_summary'}),
-        soup.findAll('span', {'data-shield-id': 'skill-text'}),
-        soup.findAll('div', {'data-shield-id': 'education_data_display'}),
-        soup.findAll('div', {'data-shield-id': 'workExperience_data_display'})
+        soup.find('span', {'data-shield-id': 'firstname'}), # First name
+        soup.find('span', {'data-shield-id': 'lastname'}), # Last name
+        soup.find('span', {'data-shield-id': 'locality'}), # Location
+        soup.find('div', {'data-shield-id': 'email'}), # Email address
+        soup.find('div', {'data-shield-id': 'phone_number'}), # Phone number
+        soup.find('div', {'data-shield-id': 'res_summary'}), # Summary
+        soup.findAll('span', {'data-shield-id': 'skill-text'}), # Skills
+        soup.findAll('div', {'data-shield-id': 'education_data_display'}), # Education
+        soup.findAll('div', {'data-shield-id': 'workExperience_data_display'}) # Work experience
         )
 
     return candidate
 
-# Iterate Through Text Elements In Document
-def parse_document_text(candidate, document_name='resumes/template.docx'):
-    document = Document(document_name)
+# Iterate through text elements in document.
+def parse_document_text(candidate, document='resumes/template.docx'):
+    document = Document(document)
 
-    # Replace Text In Paragraphs
+    # Replace text in paragraphs.
     for paragraph in document.paragraphs:
         replace_document_text(paragraph, candidate)
 
-    # Replace Text In Tables
+    # Replace text in tables.
     for table in document.tables:
         for row in table.rows:
             for cell in row.cells:
                 for paragraph in cell.paragraphs:
                     replace_document_text(paragraph, candidate)
 
-    # Save Resume As New Word Document
+    # Save resume as new word document.
     filename = candidate.first + candidate.last
     document.save('resumes/' + filename + '.docx')
 
-# Find And Replace Text In Word Document
+# Find and replace text in word document.
 def replace_document_text(element, candidate):
-    # String To Find, Replacement String
+    # replacements[x] = [string to remove, string to insert]
     replacements = [
         ['FirstName', candidate.first.upper()],
         ['LastName', candidate.last.upper()],
@@ -156,14 +152,14 @@ def replace_document_text(element, candidate):
         ['Experience', candidate.experience],
         ]
 
-    # Find Text To Replace
+    # Find text to replace
     inline = element.runs
     for i in range(len(inline)):
         for item in replacements:
             if item[0] in inline[i].text:
                 item.append(i)
 
-    # Replace Text Found
+    # Replace text found
     for replacement in replacements:
         if len(replacement) == 3:
             index = replacement[2]
@@ -171,7 +167,7 @@ def replace_document_text(element, candidate):
             replacement_text = replacement[1]
             inline[index].text = inline[index].text.replace(target_text, replacement_text)
 
-# Remove Excess White Space And Line Breaks From String
+# Remove excess white space and line breaks from string.
 def format_string(string):
     string = re.sub(' +', ' ', string)
     string = re.sub('\n', '', string)
--- parser.pyw
+++ parser.pyw
@@ -3,16 +3,16 @@ from tkinter import BooleanVar, ttk, messagebox
 
 from data import main
 
-# Pass URL To Insert Function
+# Pass url to data functions
 def submit(event = None):
-    # try:
-    main(entry_url.get(), manual_login.get())
-    # except Exception as error:
-    #     print(error)
-    #     messagebox.showerror(title='Error', message=error)
+    try:
+        main(entry_url.get(), manual_login.get())
+    except Exception as error:
+        print(error)
+        messagebox.showerror(title='Error', message=error)
     window.destroy()
 
-# Window
+# Create user interface main window
 window = tk.Tk()
 logo = tk.PhotoImage(file='resources/sourceflow.png')
 window.iconphoto(False, logo)
@@ -22,18 +22,18 @@ window.resizable(width=False, height=False)
 frame_main = ttk.Frame(master=window)
 frame_main.grid(padx=15, pady=15)
 
-# Resume URL Label, Entry
+# Create resume URL label and entry
 label_url = ttk.Label(master=frame_main, text='Resume URL:', border=1)
 label_url.grid(row=0, column=0, pady=(0, 15), sticky='w')
 entry_url = ttk.Entry(master=frame_main, width=35)
 entry_url.grid(row=0, column=1, pady=(0, 15), sticky='e')
 
-# Manual Login Checkbox
+# Create manual login checkbox
 manual_login = BooleanVar(value=False)
-checkbutton_manual_login = ttk.Checkbutton(master=frame_main, text='Reset Cookies', variable=manual_login)
+checkbutton_manual_login = ttk.Checkbutton(master=frame_main, text='Manual Login', variable=manual_login)
 checkbutton_manual_login.grid(row=1, column=0, sticky='sw')
 
-# Extract Button
+# Create extract button
 button_extract = ttk.Button(master=frame_main, text='Extract', width=10, command=submit)
 button_extract.grid(row=1, column=1, sticky='se')
 
